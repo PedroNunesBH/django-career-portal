85b71d8c18e1ad635ce0576becb7f019a24dcfad
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-18 15:55:25 -0300

    Registro do model no admin

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import JobOffer
 
-# Register your models here.
+
+class JobOfferAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'organization_name', 'location', 'publication_date', 'offer_requirements', 'employment_type', 'salary', 'organization_description')
+    search_fields = ('title',)
+
+
+admin.site.register(JobOffer, JobOfferAdmin)
