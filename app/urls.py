a640fb2b034850878e02a2762e91163dc8e21fcf
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-18 15:42:52 -0300

    Criacao da view de listagem de ofertas

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from jobs.views import JobOffersList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('job_list/', JobOffersList.as_view(), name='job_list'),
+    path('job_list/', JobOffersList.as_view(), name='job_list'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- jobs/views.py
+++ jobs/views.py
@@ -1,2 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from .models import JobOffer
+
+
+class JobOffersList(ListView):
+    template_name = 'job_list.html'
+    model = JobOffer
