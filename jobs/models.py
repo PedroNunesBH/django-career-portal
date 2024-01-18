8a0fef41901aadc63ce6c9241c3875b3792e97aa
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-18 15:40:41 -0300

    Criacao do modelo de ofertas de emprego e template base

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class JobOffer(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    organization_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    publication_date = models.DateTimeField(auto_now_add=True)  # Adicionará automaticamente a data e hora ao campo
+    offer_requirements = models.CharField(max_length=300)
+    employment_type = models.CharField(max_length=100)
+    salary = models.FloatField(default="Não informado")  # define que por padrão o campo é não informado
+    organization_description = models.TextField()
+
+    def __str__(self):
+        return self.title
