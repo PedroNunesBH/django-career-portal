69ee091299a7750978666aa5e3863d4062efbb2b
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-19 12:22:17 -0300

    Configuracao de media no settings e alteracoes de template base

--- jobs/views.py
+++ jobs/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import JobOffer
 from .forms import CreateOfferJob
+from django.urls import reverse_lazy
 
 
 class JobOffersList(ListView):
@@ -13,7 +14,7 @@ class CreateOfferView(CreateView):
     template_name = 'create_offer.html'
     model = JobOffer
     form_class = CreateOfferJob
-    success_url = 'job_list'
+    success_url = reverse_lazy('job_list')
     
     def form_valid(self, form):
         form.instance.autor = self.request.user
