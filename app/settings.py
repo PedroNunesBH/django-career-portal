8938553b4a1e5b042eecd37c404d27becb545de6
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-05-29 12:22:35 -0300

    Retorno da configuração padrão de database(sqlite) e flush no mesmo.

--- app/settings.py
+++ app/settings.py
@@ -79,15 +79,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
